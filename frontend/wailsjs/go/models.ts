38b726b166b8b5113c412021c82d4513a949097e
nguyenvanduocit <nguyenvanduocit@gmail.com>
2022-06-30 17:37:27 +0700

    WIP: Fire in the house

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,35 +1,5 @@
 export namespace app {
 	
-	export class ParseUrlResponse {
-	    scheme?: string;
-	    opaque?: string;
-	    host?: string;
-	    path?: string;
-	    raw_path?: string;
-	    force_query?: boolean;
-	    raw_query?: string;
-	    fragment?: string;
-	    raw_fragment?: string;
-	    query?: {[key: string]: string[]};
-	
-	    static createFrom(source: any = {}) {
-	        return new ParseUrlResponse(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.scheme = source["scheme"];
-	        this.opaque = source["opaque"];
-	        this.host = source["host"];
-	        this.path = source["path"];
-	        this.raw_path = source["raw_path"];
-	        this.force_query = source["force_query"];
-	        this.raw_query = source["raw_query"];
-	        this.fragment = source["fragment"];
-	        this.raw_fragment = source["raw_fragment"];
-	        this.query = source["query"];
-	    }
-	}
 	
 	
 	export class Detail {
@@ -775,6 +745,36 @@ export namespace app {
 		    }
 		    return a;
 		}
+	}
+	export class ParseUrlResponse {
+	    scheme?: string;
+	    opaque?: string;
+	    host?: string;
+	    path?: string;
+	    raw_path?: string;
+	    force_query?: boolean;
+	    raw_query?: string;
+	    fragment?: string;
+	    raw_fragment?: string;
+	    query?: {[key: string]: string[]};
+	
+	    static createFrom(source: any = {}) {
+	        return new ParseUrlResponse(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.scheme = source["scheme"];
+	        this.opaque = source["opaque"];
+	        this.host = source["host"];
+	        this.path = source["path"];
+	        this.raw_path = source["raw_path"];
+	        this.force_query = source["force_query"];
+	        this.raw_query = source["raw_query"];
+	        this.fragment = source["fragment"];
+	        this.raw_fragment = source["raw_fragment"];
+	        this.query = source["query"];
+	    }
 	}
 
 }
